ad50bc0b97be9411ede6197e9df69f219fbd33fe
hazarek <duurkak@yandex.com>
2021-01-24 07:15:00 +0300

    improvements

--- turing.py
+++ turing.py
@@ -2,48 +2,66 @@ import cv2
 import random
 import numpy as np
 import uuid
+import sys
 
-record = False
+# "q"       Quit
+# "r"       Reset simulation
+# "p" -     Save Current Frame as PNG and Quit
+# "w"/"s" - Increase/Decrease high-pass level
+
+cv2.namedWindow("Main")
+cv2.namedWindow("Difference")
+cv2.moveWindow("Difference", 0,550)
+
+
+# Create image as input
+w, h = 512, 512
+input_image = np.zeros((h, w), np.uint8)
+cv2.putText(input_image, 'RANDOM', (50, 220), 0, 3, (255))
+# video frames
 frames = []
+# Options
+record = False
 random = False
-
-# "q" Quit
-# "s" - Save Current Frame as PNG and Quit
-
-img = np.zeros((512,512,3), np.uint8)
-font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img,'RANDOM',(50,220), font, 3,(255,255,255),2,cv2.LINE_AA)
-cv2.putText(img,'TURING',(90,370), font, 3,(255,255,255),2,cv2.LINE_AA)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-rx, ry = 7, 7
-
+# Parameters
+rx, ry = 6, 6
+highpass_level = 127
+iteration = 0
+frame = input_image # hold input_image for reset.
 while True:
-    temp_img = img
+    key = cv2.waitKey(33)
+    temp_frame = frame
     if random:   
         rx, ry = random.uniform(1,3), random.uniform(1,3)
-        
-    # Reaction-diffussion filters
-    img = cv2.GaussianBlur(img, (0,0), rx, sigmaY=ry)
-    img = img - cv2.GaussianBlur(img, (0,0), sigmaX=rx, sigmaY=ry) + 115
-    ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    # Apply Reaction-diffussion filters
+    frame = cv2.GaussianBlur(frame, (0,0), rx, sigmaY=ry)
+    frame = frame - cv2.GaussianBlur(frame, (0,0), sigmaX=rx, sigmaY=ry) + highpass_level
+    ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+    # show
+    cv2.imshow("Difference", cv2.subtract(frame, temp_frame))
+    cv2.imshow("Simulation", frame)
     
-    key = cv2.waitKey(1)
-    if key == ord('s'):
-        cv2.imwrite("out" + str(uuid.uuid4()) + ".png", img)
+    if key == ord('p'):
+        cv2.imwrite("test/out_" + str(uuid.uuid4()) + ".png", frame)
         break
     if key==ord('q'):
         break
+    if key==ord('r'): #reset simulation
+        frame = input_image
+    if key==ord('w'):
+        highpass_level += 1
+        print(highpass_level)
+    if key==ord('s'):
+        highpass_level -= 1
+        print(highpass_level)
     if record:
-        frames.append(img)
-    # cv2.imshow("RD", cv2.subtract(img, temp_img))
-    cv2.imshow("RD", img)
-    
-
+        frames.append(frame)
+    iteration += 1
 if record:
     out = cv2.VideoWriter("out.mov",
     cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
     23,
-    (img.shape[1], img.shape[0]))
+    (frame.shape[1], frame.shape[0]))
     for i in range(len(frames)):
         out.write(frames[i])
     out.release()
